3a101e5c5e4c6f3d45e7151952be346e52d29803
amanuelgthn <amanuelgthn@gmail.com>
2023-05-30 21:32:55 +0800

    update 1-binary_tree_insert_left.c

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -13,13 +13,11 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	if (new == NULL)
 		return (NULL);
 	new = binary_tree_node(parent, value);
-	if (parent->left == NULL)
-		parent->left = new;
-	else
+	if (parent->left != NULL)
 	{
 		parent->left->parent = new;
 		new->left = parent->left;
-		parent->left = new;
 	}
+	parent->left = new;
 	return (new);
 }
