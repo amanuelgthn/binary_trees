b8089cef8797ec404946ccaa03b699bc593decde
amanuelgthn <53990706+amanuelgthn@users.noreply.github.com>
2023-05-30 14:14:36 +0800

    Update binary_trees.h

--- binary_trees.h
+++ binary_trees.h
@@ -18,6 +18,7 @@ struct binary_tree_s
     struct binary_tree_s *left;
     struct binary_tree_s *right;
 };
+void binary_tree_print(const binary_tree_t *);
 typedef struct binary_tree_s binary_tree_t;
 typedef struct binary_tree_s bst_t;
 typedef struct binary_tree_s avl_t;
