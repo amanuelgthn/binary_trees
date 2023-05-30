003763996346c43b10e51f5097332d620e400907
amanuelgthn <amanuelgthn@gmail.com>
2023-05-30 22:02:52 +0800

    Update 2-binart_tree_insert_right.c remove spaces

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -1,14 +1,15 @@
 #include"binary_trees.h"
 /**
-*binary_tree_insert_right-function that inserts a node as right child of anothere node
+*binary_tree_insert_right-function that inserts a
+*node as right child of another node
 *@parent:a pointer to the node to insert the right-child in
 *@value:value to store in the new node
 *Return:pointer to the created node or NULL on failure or if parent is NULL
 **/
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
-	binary_tree_t new;
-	
+	binary_tree_t *new;
+
 	if (parent == NULL)
 		return (NULL);
 	new = binary_tree_node(parent, value);
