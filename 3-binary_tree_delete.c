b54abec807ec1d8c7829bbabd982dfcc5372b339
amanuelgthn <amanuelgthn@gmail.com>
2023-05-30 23:31:18 +0800

    Update 3-binary_tree_delete.c

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -9,5 +9,6 @@ void binary_tree_delete(binary_tree_t *tree)
 		return;
 	binary_tree_delete(tree->left);
 	binary_tree_delete(tree->right);
-	binary_tree_delete(tree->parent);
+	free(tree->parent);
 	free(tree);
+}
