8838be910c9e0e03253f4896476880a4c6ddc967
amanuelgthn <53990706+amanuelgthn@users.noreply.github.com>
2023-05-30 23:27:46 +0800

    Update 3-binary_tree_delete.c

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -6,7 +6,7 @@
 void binary_tree_delete(binary_tree_t *tree)
 {
 	if (tree == NULL)
-		return (NULL);
+		return;
 	binary_tree_delete(tree->left);
 	binary_tree_delete(tree->right);
 	binary_tree_delete(tree->parent);
