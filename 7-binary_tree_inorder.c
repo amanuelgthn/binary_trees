e90715b3fb8a13739814e44ba882a34eb6e71505
amanuelgthn <53990706+amanuelgthn@users.noreply.github.com>
2023-05-31 15:59:47 +0800

    Create 7-binary_tree_inorder.c

---
+++ 7-binary_tree_inorder.c
@@ -0,0 +1,18 @@
+#include"binary_trees.h"
+//**
+**binary_tree_inorder-a function that goes through a binary tree
+*using an iorder traversal
+*@tree:binary tree to be traversed in order
+*@func:a pointer to a function to call for each node
+*Do nothing if tree or func is NULL
+**/
+void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (tree == NULL || func == NULL)
+		return;
+	if (tree->left)
+		binary_tree_inorder(tree->left, func);
+	func(tree);
+	if (tree->right)
+		binary_tree_inorder(tree->right, func);
+	
