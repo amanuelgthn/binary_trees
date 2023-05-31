a960396a855b70bc6ffd575360ed2c34f37b72f4
amanuelgthn <amanuelgthn@gmail.com>
2023-06-01 06:44:07 +0800

    update 11-binary_tree_size.c

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -6,32 +6,8 @@
 **/
 size_t binary_tree_size(const binary_tree_t *tree)
 {
-	size_t size;
+	if (tree == NULL)
+		return (0);
+	return (binary_tree_size(tree->left) + 1 + binary_tree_size(tree->right));
+}
 
-	if (tree == NULL)
-		return (0);
-	size = binary_tree_size_recur(tree, 1);
-	return (size);
-}
-/**
- * binary_tree_size_recur-function that measures the size of binary tree recursively
- * @tree:a pointer to the root node of the tree to measure the size
- * @size:size of the tree to be measured
- * Return:the size of the binary tree
-*/
-size_t binary_tree_size_recur(const binary_tree_t *tree, size_t size)
-{
-	if (tree == NULL)
-		return (0);
-	if (tree->left)
-	{
-		size = size + 1;
-		binary_tree_size_recur(tree->left, size);
-	}
-	if (tree->right)
-	{
-		size = size + 1;
-		binary_tree_size_recur(tree->right, size);
-	}
-	return (size);
-}
