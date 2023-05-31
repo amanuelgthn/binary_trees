b214e842315f39b6d36069487c012d2e47517a61
amanuelgthn <amanuelgthn@gmail.com>
2023-06-01 06:54:52 +0800

    create 12-buinary_tree_leaves.c" git push

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -1,7 +1,16 @@
-#include"binary_trees"
+#include"binary_trees.h"
 /**
+ * binary_trees_leaves-function that counts the leaves in a binary trees
+ * @tree:a ointer to the root node of the tree to count the number of leaves
+ * Return: number of leaves of the given node or 0 if the tree is NULL
 **/
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
-	const je;
+	size_t num_leave = 0;
+
+	if(tree->left)
+		num_leave += 1;
+	if(tree->right)
+		num_leave += 1;
+	return (num_leave);
 }
