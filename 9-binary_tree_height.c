d58f228d5ac5ebfd0d9fb9cc8ebb77d0ec8cea87
amanuelgthn <amanuelgthn@gmail.com>
2023-05-31 19:01:17 +0800

    update 9-binary_tree_height.c

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -10,15 +10,15 @@ size_t binary_tree_height(const binary_tree_t *tree)
 
 	if (tree == NULL)
 		return (0);
-	if(tree->left != NULL)
+	if (tree->left != NULL)
 	{
 		left += 1;
 		binary_tree_height(tree->left);
 	}
-	if(tree->right != NULL)
+	if (tree->right != NULL)
 	{
-		right += 1;
-		binary_tree_height(tree->left);
+		left += 1;
+		binary_tree_height(tree->right);
 	}
 	if (left > right)
 		return (left);
