651b748cf876c13cc24c661771dff74ce3ffbd51
amanuelgthn <53990706+amanuelgthn@users.noreply.github.com>
2023-05-31 11:21:17 +0800

    Create 6-binary_tree_preorder.c

---
+++ 6-binary_tree_preorder.c
@@ -0,0 +1,9 @@
+#include"binary_trees.h"
+/**
+*binary_tree_preorder-a function that goes through a binary tree using pre-order traversal
+*@tree:
+*@func:
+**/
+void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if
