8570d7815cc3d60b16b86b869996376be6bce4b0
amanuelgthn <53990706+amanuelgthn@users.noreply.github.com>
2023-06-01 09:45:36 +0800

    Update 13-binary_tree_nodes.c

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -8,6 +8,8 @@ size_t binary_tree_nodes(const binary_tree_t *tree)
 {
 	if (tree == NULL)
 		return (0);
+	if(tree->left == NULL || tree->right == NULL)
+		return (1);
 	else
 		return (binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right) + 1);
 }
