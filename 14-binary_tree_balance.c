85a373f12fb287875efb9ca95ae6484eb33cdfce
amanuelgthn <53990706+amanuelgthn@users.noreply.github.com>
2023-06-01 11:47:02 +0800

    Update 14-binary_tree_balance.c

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -24,11 +24,6 @@ size_t binary_tree_height(const binary_tree_t *tree)
 **/
 int binary_tree_balance(const binary_tree_t *tree)
 {
-	int left = 0, right = 0;
-
-	left = binary_tree_height(tree->left);
-	right = binary_tree_height(tree->right);
-
-	return (left - right);
+	return (binary_tree_height(tree->left) - binary_tree_height(tree->right));
 }
 	
