728be51cda0cda1f67b3a08b162e734cee92e102
amanuelgthn <53990706+amanuelgthn@users.noreply.github.com>
2023-06-01 15:54:43 +0800

    Update 15-binary_tree_is_full.c

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -1,4 +1,4 @@
-#include"binary_rees.h"
+#include"binary_trees.h"
 /**
 *binary_tree_is_full-functions that checks if a binary tree is full
 *@tree: a pointer to the root node of the tree to be checked
