da77bb684b1ce916b1e42645747bd1a28f3bbc7a
amanuelgthn <53990706+amanuelgthn@users.noreply.github.com>
2023-06-03 12:30:53 +0800

    Update 103-binary_tree_rotate_left.c

--- 103-binary_tree_rotate_left.c
+++ 103-binary_tree_rotate_left.c
@@ -1,5 +1,23 @@
 #include"binary_trees.h"
 /**
+*binary_tree_node-function that creates a binary tree node
+*@parent: a pointer to the parent node of the node to be created
+*@value: the value to be put in the new node
+*Return:return a pointer to the new node or NULL on failure
+**/
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+	binary_tree_t *new = malloc(sizeof(binary_tree_t));
+
+	if (new == NULL)
+		return (NULL);
+	new->parent = parent;
+	new->n = value;
+	new->left = NULL;
+	new->right = NULL;
+	return (new);
+}
+/**
 *binary_tree_rotate_left-function that performs
 *a left-rotation on a binary tree
 *@tree:a pointer to the root node of the tree to rotate
