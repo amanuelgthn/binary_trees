55ac161a644345956b47f75c27d0948c9fa34696
amanuelgthn <amanuelgthn@gmail.com>
2023-06-03 09:58:00 +0800

    Update 101-binary_tree_levelorder.c

--- 101-binary_tree_levelorder.c
+++ 101-binary_tree_levelorder.c
@@ -1,35 +1,4 @@
 #include"binary_trees.h"
-void binary_tree_print_level_order(const binary_tree_t *node,
-int level)
-{
-	if (node == NULL)
-		return;
-	if (level == 1)
-		printf("%d\n", node->n);
-	else if (level > 1)
-	{
-		binary_tree_print_level_order(node->left, level - 1);
-		binary_tree_print_level_order(node->right, level - 1);
-	}
-}
-/**
- *binary_tree_height-functioin that measures the height of a binary tree
- *@tree:a pointer to the root node of the tree to measure the height
- *Return:the height of the binary tree or 0 if tree is NULL
- **/
-size_t binary_tree_height(const binary_tree_t *tree)
-{
-	size_t left_height, right_height;
-
-	if (tree == NULL || (tree->left == NULL && tree->right == NULL))
-		return (0);
-	left_height = binary_tree_height(tree->left);
-	right_height = binary_tree_height(tree->right);
-
-	if (left_height >= right_height)
-		return (left_height + 1);
-	return (right_height + 1);
-}
 /**
  *binary_tree_levelorder-function that goes through
  *a binary tree using level-order traversal
@@ -41,8 +10,9 @@ void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int))
 {
 	if (tree == NULL || func == NULL)
 		return;
-	int height = binary_tree_height(tree) + 1, i;
-
-	for (i = 1; i <= height; i++)
-		binary_tree_print_level_order(tree, i);
+	if (tree->left)
+		binary_tree_inorder(tree->left, func);
+	func(tree->n);
+	if (tree->right)
+		binary_tree_inorder(tree->right, func);
 }
