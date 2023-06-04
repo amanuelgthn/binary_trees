014c2e82ff9ace666cf160c8e0065d733b5f6eec
amanuelgthn <amanuelgthn@gmail.com>
2023-06-04 11:37:57 +0800

    UPdate 103-binary_tree_rotate_left.c

---
+++ null.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "binary_trees.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	binary_tree_t *root;
+
+	root = binary_tree_rotate_left(NULL);
+	printf("Root: %p\n", (void *)root);
+
+	return (0);
+}
