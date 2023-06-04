b627bbf6fc09bc5d3808f2e52d7359135fbc5d15
amanuelgthn <amanuelgthn@gmail.com>
2023-06-04 10:57:41 +0800

    update 103-binary_tree_rotate_left.c

--- 103-binary_tree_rotate_left.c
+++ 103-binary_tree_rotate_left.c
@@ -1,5 +1,19 @@
 #include"binary_trees.h"
 /**
+*binary_tree_delete-function that deletes an entire binary tree
+*@tree:a pointer to the root node of the tree to delete
+**/
+void binary_tree_delete(binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return;
+	if (tree->left)
+		binary_tree_delete(tree->left);
+	if (tree->right)
+		binary_tree_delete(tree->right);
+	free(tree);
+}
+/**
 *binary_tree_rotate_left-function that performs
 *a left-rotation on a binary tree
 *@tree:a pointer to the root node of the tree to rotate
@@ -18,5 +32,6 @@ binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree)
 	new->left->n = tree->n;
 	new->left->right = tree->right->left;
 	new->left->left = tree->left;
+	tree->right = new;
 	return (new);
 }
