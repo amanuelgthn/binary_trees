0f3014b6b6f7a6673f579ec98eadeb51cf3dc31f
amanuelgthn <amanuelgthn@gmail.com>
2023-06-04 15:21:51 +0800

    Update 110-binary_tree_is_bst.c

--- 110-binary_tree_is_bst.c
+++ 110-binary_tree_is_bst.c
@@ -9,13 +9,17 @@ int binary_tree_is_bst(const binary_tree_t *tree)
 {
 	if (tree == NULL)
 		return (0);
-	if (tree->left->n >= tree->n || tree->left->n >= tree->right->n)
-		return (0);
-	/**
-	 * if (tree->left)
-		*binary_tree_is_bst(tree->left);
-	*if (tree->right)
-		*binary_tree_is_bst(tree->right);
-		**/
+	if (tree->left)
+	{
+		if (tree->left->n >= tree->n)
+			return (0);
+		if (tree->right)
+		{
+			if (tree->left->n >= tree->right->n)
+				return (0);
+		}
+	}
+	binary_tree_is_bst(tree->left);
+	binary_tree_is_bst(tree->right);
 	return (1);
 }
