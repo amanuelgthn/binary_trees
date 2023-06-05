bea5a3a4e050cefddc9668fed5ea954a85676759
amanuelgthn <53990706+amanuelgthn@users.noreply.github.com>
2023-06-05 19:51:13 +0800

    Update 102-binary_tree_is_complete.c

--- 102-binary_tree_is_complete.c
+++ 102-binary_tree_is_complete.c
@@ -1,7 +1,7 @@
 #include"binary_trees.h"
 /**
 *binary_tree_size-function that measures the size of a binary tree
-*@tree:a pointer to the root node of the ree to measure the size
+*@tree: a pointer to the root node of the ree to measure the size
 *Return:the size of the binary_tree or 0 if the tree is NULL
 **/
 size_t binary_tree_size(const binary_tree_t *tree)
